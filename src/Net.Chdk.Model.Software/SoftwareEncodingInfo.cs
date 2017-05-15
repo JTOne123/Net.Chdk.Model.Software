5ab611bcee113614b1a38fffcdc9826eb131553b
Dmitry Shechtman <damnian@gmail.com>
2017-05-15 04:59:10 +0300

    Add encoding data converter

--- src/Net.Chdk.Model.Software/SoftwareEncodingInfo.cs
+++ src/Net.Chdk.Model.Software/SoftwareEncodingInfo.cs
@@ -1,8 +1,13 @@
-﻿namespace Net.Chdk.Model.Software
+﻿using Net.Chdk.Json;
+using Newtonsoft.Json;
+
+namespace Net.Chdk.Model.Software
 {
     public sealed class SoftwareEncodingInfo
     {
         public string Name { get; set; }
+
+        [JsonConverter(typeof(HexStringJsonConverter))]
         public ulong? Data { get; set; }
     }
 }
