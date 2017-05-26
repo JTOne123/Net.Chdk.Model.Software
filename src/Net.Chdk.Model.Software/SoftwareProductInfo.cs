d0d9399dd4a1ba8445c3aec6eb7578ac3675119b
Dmitry Shechtman <damnian@gmail.com>
2017-05-26 20:16:43 +0300

    Add Category

--- src/Net.Chdk.Model.Software/SoftwareProductInfo.cs
+++ src/Net.Chdk.Model.Software/SoftwareProductInfo.cs
@@ -6,6 +6,7 @@ namespace Net.Chdk.Model.Software
     public sealed class SoftwareProductInfo
     {
         public string Name { get; set; }
+        public string Category { get; set; }
         public Version Version { get; set; }
         public DateTime? Created { get; set; }
         public CultureInfo Language { get; set; }
