4b4988dbfc3b769b33a51b4224bb01600f5239fa
Dmitry Shechtman <damnian@gmail.com>
2017-05-27 17:47:48 +0300

    Add Creator

--- src/Net.Chdk.Model.Software/SoftwareBuildInfo.cs
+++ src/Net.Chdk.Model.Software/SoftwareBuildInfo.cs
@@ -5,5 +5,6 @@
         public string Name { get; set; }
         public string Status { get; set; }
         public string Changeset { get; set; }
+        public string Creator { get; set; }
     }
 }
