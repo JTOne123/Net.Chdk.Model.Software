915e2e3c7c967f9c2df47e75c0ce5fe51c014613
Dmitry Shechtman <damnian@gmail.com>
2017-05-29 20:39:35 +0300

    Add ProductName

--- src/Net.Chdk.Model.Software/ModulesInfo.cs
+++ src/Net.Chdk.Model.Software/ModulesInfo.cs
@@ -6,6 +6,7 @@ namespace Net.Chdk.Model.Software
     public sealed class ModulesInfo
     {
         public Version Version { get; set; }
+        public string ProductName { get; set; }
         public IDictionary<string, ModuleInfo> Modules { get; set; }
     }
 }
