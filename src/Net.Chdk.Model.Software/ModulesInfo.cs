c0226809e020b44c750f74a4e86e5ca277c669e2
Dmitry Shechtman <damnian@gmail.com>
2017-05-29 17:29:24 +0300

    Add ModulesInfo Add ModuleInfo

---
+++ src/Net.Chdk.Model.Software/ModulesInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Model.Software
+{
+    public sealed class ModulesInfo
+    {
+        public Version Version { get; set; }
+        public IDictionary<string, ModuleInfo> Modules { get; set; }
+    }
+}
