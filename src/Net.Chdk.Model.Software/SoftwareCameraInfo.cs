e12be7d9a8b3cda394f2ff7011d7245c88929642
Dmitry Shechtman <damnian@gmail.com>
2017-06-29 05:39:57 +0300

    Add Equals() and GetHashCode() to SoftwareCameraInfo

--- src/Net.Chdk.Model.Software/SoftwareCameraInfo.cs
+++ src/Net.Chdk.Model.Software/SoftwareCameraInfo.cs
@@ -4,5 +4,16 @@
     {
         public string Platform { get; set; }
         public string Revision { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var camera2 = obj as SoftwareCameraInfo;
+            return Platform.Equals(camera2?.Platform) && Revision.Equals(camera2?.Revision);
+        }
+
+        public override int GetHashCode()
+        {
+            return Platform.GetHashCode() ^ Revision.GetHashCode();
+        }
     }
 }
