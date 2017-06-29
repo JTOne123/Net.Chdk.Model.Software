6a863bcd183e26d0c2756cafd207a356804615ac
Dmitry Shechtman <damnian@gmail.com>
2017-06-29 05:39:39 +0300

    Add Platform to SoftwareCompilerInfo

--- src/Net.Chdk.Model.Software/SoftwareCompilerInfo.cs
+++ src/Net.Chdk.Model.Software/SoftwareCompilerInfo.cs
@@ -5,6 +5,7 @@ namespace Net.Chdk.Model.Software
     public sealed class SoftwareCompilerInfo
     {
         public string Name { get; set; }
+        public string Platform { get; set; }
         public Version Version { get; set; }
     }
 }
