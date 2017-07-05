cb4c39bd3ae21e0baed1c1962902d162e5dd49f7
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 06:40:17 +0300

    Override Equals() and GetHashCode() in SoftwareSourceInfo

--- src/Net.Chdk.Model.Software/SoftwareSourceInfo.cs
+++ src/Net.Chdk.Model.Software/SoftwareSourceInfo.cs
@@ -5,5 +5,18 @@
         public string Name { get; set; }
         public string Channel { get; set; }
         public System.Uri Url { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var source2 = obj as SoftwareSourceInfo;
+            if (source2 == null)
+                return false;
+            return Name.Equals(source2.Name) && Channel.Equals(source2.Channel) && Url.Equals(source2.Url);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Channel.GetHashCode() ^ Url.GetHashCode();
+        }
     }
 }
